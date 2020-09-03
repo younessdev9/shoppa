9d8ed51ef49338d2a765c32734876d0d3c995a1d
younessdev9 <hassani.youness1999@gmail.com>
2020-09-03 23:19:45 +0100

    adding data to the app and imeges

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import data from './data.json';
+import Products from './components/products';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping</a>
-      </header>
-      <main>products List</main>
-      <footer>All rights reserved</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All rights reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
